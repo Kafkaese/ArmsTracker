5d3819a2b491720a627c0a74c8026fd97cd293e0
Frederik Laubisch <kafkaese@gmail.com>
2022-03-04 14:08:52 +0100

    test path on heroku with changed path

--- app.py
+++ app.py
@@ -3,18 +3,8 @@ import streamlit as st
 import geopandas
 import plotly.express as px
 
-import subprocess
 
-et = subprocess.run(["ls"], capture_output=True)
-st.write(et)
-
-et = subprocess.run(["pwd"], capture_output=True)
-st.write(et)
-
-#subprocess.run(["cd data/geo && pwd"])
-
-
-#world = geopandas.read_file('/data/geo/world.geojson')
+world = geopandas.read_file('data/geo/world.geojson')
 
 #world.drop(columns = ['zoneName', 'countryKey'], inplace = True)
 
