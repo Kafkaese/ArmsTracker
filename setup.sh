425be8b43862c57fc4aaf8000cc3f4154d7864a3
Frederik Laubisch <kafkaese@gmail.com>
2022-03-04 08:07:57 +0100

    add Procfile, setup.sh and app.py

---
+++ setup.sh
@@ -0,0 +1,19 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]
+\
+email = \"your-email@domain.com\"
+\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]
+\
+headless = true
+\
+enableCORS=false
+\
+port = $PORT
+\
+" > ~/.streamlit/config.toml
