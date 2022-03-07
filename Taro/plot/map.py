1ef5c9c5b464daecf7ea4043b181103d1e53bfbe
Frederik Laubisch <kafkaese@gmail.com>
2022-03-07 16:18:05 +0100

    add new palette and color mapping

--- Taro/plot/map.py
+++ Taro/plot/map.py
@@ -2,8 +2,10 @@ from bokeh.plotting import figure
 from bokeh.models import GeoJSONDataSource
 from bokeh.palettes import brewer
 
+from bokeh.io import show
 from bokeh.io.doc import curdoc
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, LinearColorMapper, WheelZoomTool
+
 from bokeh.layouts import column
 
 def plot_export_map(data, **kwargs):
@@ -19,8 +21,11 @@ def plot_export_map(data, **kwargs):
     geosource = GeoJSONDataSource(geojson = data)
 
     # pick palette
-    palette = brewer['Blues'][8]
-    palette = palette[::-1]
+    palette = brewer['RdYlGn'][11]
+    #palette = palette[::-1]
+    
+    # Color mapper
+    color_mapper = LinearColorMapper(palette = palette, low = 0, high = 1000)
     
     # Create figure
     p = figure(title = 'Title', 
@@ -31,17 +36,19 @@ def plot_export_map(data, **kwargs):
     p.axis.visible = False
 
     # Add patch renderer to figure. 
-    p.patches('xs','ys', source = geosource,
-          line_color = 'black', line_width = 0.25, fill_alpha = 1)
+    p.patches('xs','ys', source = geosource, fill_color = {'field' : 'total_mil', 'transform' : color_mapper}, line_color = 'black', line_width = 0.25, fill_alpha = 1)
 
     # Specify color bar layout.
     #p.add_layout(color_bar, 'right')
 
-    # Add hover tool
+    # Inst tools
     hover = HoverTool(tooltips = [ ('Name','@countryName'), ('Total Exports 2020 (Million EUR)', '@total_mil')])
-
+    zoom = WheelZoomTool()
+    
     # Add the hover tool to the graph
     p.add_tools(hover)
-    
+    p.add_tools(zoom)
+        
     layout = column(p)
     curdoc().add_root(layout)
+    
