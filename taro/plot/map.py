da85c5b38ede22b807369fc79c3d58b900195f53
Frederik Laubisch <kafkaese@gmail.com>
2022-03-10 12:18:57 +0100

    fix amount to millions

--- taro/plot/map.py
+++ taro/plot/map.py
@@ -56,7 +56,7 @@ def plot_export_map(data, **kwargs):
             </div>
             <div>
                 <span style="font-size: 15px;">Total Exports (mil EUR)</span>
-                <span style="font-size: 10px; color: #696;">@value</span>
+                <span style="font-size: 10px; color: #696;">@total_mil</span>
             </div>
         </div>
     """
